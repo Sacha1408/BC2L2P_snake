017b22b1058bae2fc59867d0d94ee2f2b240c6cf
Alexandre PELLETIER <aex4236@gmail.com>
2021-09-01 13:33:10 +0200

    refactoring murs et pause quand collision

--- Assets/Scripts/Entities/Obstacle.cs
+++ Assets/Scripts/Entities/Obstacle.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class Obstacle : Entity 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // Constructeur reprenant le constructeur de l'entité
     public Obstacle(Vector3 p) : base() {}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("in collision");
+
+        // Si c'est une collision avec le joueur
+        if (collision.gameObject.tag == "Player")
+        {
+            // Game over
+            Debug.Log("Game over");
+
+            // On arrête le temps
+            Time.timeScale = 0f;
+        }
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("in trigger");
+
+        // Si c'est une collision avec le joueur
+        if (c.gameObject.tag == "Player")
+        {
+            // Game over
+            Debug.Log("Game over");
+
+            // On arrête le temps
+            Time.timeScale = 0f;
+        }
+    }
 }
